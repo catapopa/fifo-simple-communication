e1f198bdab2f769b6fbd8019fee4889b5f4d14c4
Cata Popa <cataqoq@gmail.com>
2019-05-02 18:41:41 +0300

    uploaded files - first commit

---
+++ fifo.c
@@ -0,0 +1,104 @@
+// proces 1: citeste de la intrarea standard si scrie:
+// 1.intrare: toate liniile care incep cu o litera
+// 2.intrare: toate liniile care incep cu o cifre
+// 3.intrare: restul liniilor
+// proces 2,3,4: afiseaza ceea ce primesc
+
+
+#include "header.h"
+
+int is_digit (char c){
+	if ( c >= '0' && c <= '9')
+		return 1;
+	return 0;
+}
+
+int is_alpha (char c){
+	if ( c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return 1;
+	return 0;
+}
+
+void create_fifo(char* file_name){
+	if ((mkfifo(file_name, 0666)) && (errno!=EEXIST)){
+		printf("error creating fifo\n");
+		exit(0);
+	}
+	printf(CYN "fifo created\n");
+}
+
+void delete_fifo(char* file_name){
+	if(unlink(file_name) < 0){
+		printf("error deleting fifo\n");
+		exit(0);
+	}
+	printf(CYN "fifo deleted\n");
+}
+
+void read_fifo(char* file_name){
+	printf(GRN "%s\n", file_name);
+	char str[31];
+	int fs;
+	fs = open(file_name, O_RDONLY);
+	read(fs, str, 31);
+	printf(CYN "%s   \n", str);
+}
+
+void read_line(int fl, int fn, int fs){
+	// read string, write in fifo
+	int ok = 0, *linii;
+	char str[31];
+
+	do{
+		printf(MAG "Enter max 30 characters: ");
+		scanf("%s", str);
+		str[strlen(str)+1]='\0';
+		printf("\n");
+		// write in fifo
+		if (is_alpha(str[0])){
+			write(fl, str, strlen(str));
+		}else if (is_digit(str[0])){
+			write(fn, str, strlen(str));
+		}else{
+			write(fs, str, strlen(str));
+		}
+		printf(YEL "Read another line? 1/0 ");
+		scanf("%d", &ok);
+	}while(ok == 1);
+}
+
+int main(){
+
+	int fl, fn, fs, ok = 0;
+
+	printf("START-%d\n", getpid());
+
+	// create fifo
+	create_fifo(FLETTER);
+	create_fifo(FNUMBER);
+	create_fifo(FSOMETHING);
+	
+	// open fifo
+	fl = open(FLETTER, O_RDWR);
+	fn = open(FNUMBER, O_RDWR);
+	fs = open(FSOMETHING, O_RDWR);
+
+	// read string, write in fifo
+	read_line(fl, fn, fs);
+
+	// read from fifo, print string
+	read_fifo(FLETTER);
+	read_fifo(FNUMBER);
+	read_fifo(FSOMETHING);
+
+	close(fl);
+	close(fn);
+	close(fs);
+
+	// delete fifo
+	delete_fifo(FLETTER);
+	delete_fifo(FNUMBER);
+	delete_fifo(FSOMETHING);
+
+	return 0;
+}
---
+++ header.h
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/errno.h>
+
+#define FLETTER "letter"
+#define FNUMBER "number"
+#define FSOMETHING "something"
+
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+extern int errno;
